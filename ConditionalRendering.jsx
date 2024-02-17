a2a5bcc4b3c9a56c8665239bc7bff8dcd0907cf1
Programwithshubham <114507544+Programwithshubham@users.noreply.github.com>
2024-02-17 23:10:38 +0530

    Add files via upload
    today learn about Conditional Rendering in React

---
+++ ConditionalRendering.jsx
@@ -0,0 +1,36 @@
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [todo, setTodo] = useState([
+//     {
+//       title: "Todo 1",
+//       desc: "i am Shubham",
+//     },
+//     {
+//       title: "Todo 2",
+//       desc: "i am lokesh",
+//     },
+//     {
+//       title: "Todo 3",
+//       desc: "i am elvish",
+//     },
+//     {
+//       title: "Todo 4",
+//       desc: "i am abhishek",
+//     },
+//   ]);
+
+//   return (
+//     <div>
+//       {todo.map((item) => (
+//         <div key={item.title}>
+//           <div>{item.title}</div>
+//           <div>{item.desc}</div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default App;
